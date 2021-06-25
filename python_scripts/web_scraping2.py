e9dbd0bcceff70789001ef085d7bbc1fa225cfe4
armanshz <85336907+armanshz@users.noreply.github.com>
2021-06-25 14:38:44 +0800

    Update web_scraping2.py

--- python_scripts/web_scraping2.py
+++ python_scripts/web_scraping2.py
@@ -9,7 +9,7 @@ soup = BeautifulSoup(response.text,"lxml")
 items = soup.find_all("div",class_="col-lg-4 col-md-6 mb-4")
 count=1
 for i in items:
-    itemName = i.find("h4",class_="card-title").text
+    itemName = i.find("h4",class_="card-title").text.strip("\n")
     itemPrice = i.find("h5").text
     print(count,") price: ",itemPrice,", item name: ", itemName)
     count+=1
