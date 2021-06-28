651c4bf6e1cf28aa36d51682e6cd5f498c8c35da
armanshz <85336907+armanshz@users.noreply.github.com>
2021-06-28 14:25:05 +0800

    Update linear_binary_search.py

--- data_structures_algo/linear_binary_search.py
+++ data_structures_algo/linear_binary_search.py
@@ -1,51 +1,45 @@
-# 
 # Linear search algorithm
 
-# 
-
-# def linear_search(list, target):
-#     for i in range(0, len(list)):
-#         if list[i] == target:
-#             return i
-#     return None
+def linear_search(list, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
 
 
-# def verify(target):
-#     if target is not None:
-#         print("Target found at index", target)
-#     else:
-#         print("Target not found in list")
+def verify(target):
+    if target is not None:
+        print("Target found at index", target)
+    else:
+        print("Target not found in list")
 
-# numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,2,3,4,5,6,7,8,9,10]
 
-# result = linear_search(numbers, 12)
-# verify(result)
+result = linear_search(numbers, 12)
+verify(result)
 
-# result = linear_search(numbers, 6)
-# verify(result)
+result = linear_search(numbers, 6)
+verify(result)
 
 
 # type 1 Binary Search  - LIST NEEDS TO BE SORTED IN ASCENDING
-# numbers = [1,2,3,4,5,6,7,8,9,10]
 
-# def binary_search(list, target):
+def binary_search(list, target):
 # first/last refers to indices of the list
-#     first, last = 0, len(list) - 1
+    first, last = 0, len(list) - 1
 
-#     while (first <= last):
-#         midpoint = (first+last)//2 # // is floor division operator
+    while (first <= last):
+        midpoint = (first+last)//2 # // is floor division operator
 
-#         if list[midpoint] == target:
-#             return midpoint
-#         elif list[midpoint] < target:
-#             first = midpoint + 1
-#         else:
-#             last = midpoint - 1
-#     return None
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return None
 
 # type 2 RECURSIVE binary search
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
 
 def binary_search_recursive(list, target):
     if len(list) == 0:
@@ -67,4 +61,3 @@ result = binary_search_recursive(numbers,12)
 verify(result)
 result = binary_search_recursive(numbers,6)
 verify(result)
-
